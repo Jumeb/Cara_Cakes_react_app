51e75d4c981fb587ec9b70baf3d690c8cd3695b2
Jumeb <bricejume@gmail.com>
2021-04-19 21:17:03 +0100

    Adjusted responsiveness

--- src/sections/Footer/Footer.section.js
+++ src/sections/Footer/Footer.section.js
@@ -47,13 +47,14 @@ const Footer = () => {
         </div>
     </div>
     <div className={styles.footerContacts}>
-        <span className={styles.footerContactsRound}><a href='https://www.facebook.com/jume.njah' target="_blank"><IoLogoFacebook className={styles.footerIcons}/></a></span>
+        <span className={styles.footerContactsRound}><a href='https://www.facebook.com/jume.njah' target="_blank" rel="noreferrer"><IoLogoFacebook className={styles.footerIcons}/></a></span>
         <span className={styles.footerContactsRound}><a href='tel:237679574561'><IoPhonePortrait className={styles.footerIcons}/></a></span>
         <span className={styles.footerContactsRound}><a href='mailto:bricejume@gmail.com'><IoMail className={styles.footerIcons}/></a></span>
         <span className={styles.footerContactsRound}>
             <a 
                 href="https://wa.me/237681726633?text=Good%20day,%20Mr%20Jume%20Brice,%20I'm%20interested%20in%20creating%20a%20baker%20account%20for%20my%20pastry%20business"
-                target="_blank"
+                        target="_blank"
+                        rel="noreferrer"
             >
                 <IoLogoWhatsapp />
             </a></span>
